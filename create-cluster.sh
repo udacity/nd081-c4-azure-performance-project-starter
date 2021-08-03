a249d3d41f80df6b3c5d7afe4d263a5672648e4f
SudKul <sudhanshu.kulshrestha@gmail.com>
2021-08-03 14:32:47 +0530

    Update script for Cloud Lab Users

--- create-cluster.sh
+++ create-cluster.sh
@@ -12,8 +12,10 @@ az aks install-cli
 echo "AKS CLI installed"
 
 # Create AKS cluster
-echo "Creating AKS cluster $clusterName"
-
+echo "Step 1 - Creating AKS cluster $clusterName"
+# Use either one of the "az aks create" commands below
+# For users working in their personal Azure account
+# This commmand will not work for the Cloud Lab users, because you are not allowed to create Log Analytics workspace for monitoring
 az aks create \
 --resource-group $resourceGroup \
 --name $clusterName \
@@ -21,11 +23,24 @@ az aks create \
 --enable-addons monitoring \
 --generate-ssh-keys
 
+# For Cloud Lab users
+az aks create \
+--resource-group $resourceGroup \
+--name $clusterName \
+--node-count 1 \
+--generate-ssh-keys
+
+# For Cloud Lab users
+# This command will is a substitute for "--enable-addons monitoring" option in the "az aks create"
+# Use the log analytics workspace - Resource ID
+# For Cloud Lab users, go to the existing Log Analytics workspace --> Properties --> Resource ID. Copy it and use in the command below.
+az aks enable-addons -a monitoring -n $clusterName -g $resourceGroup --workspace-resource-id "/subscriptions/6c39f60b-2bb1-4e37-ad64-faaf30beaca4/resourcegroups/cloud-demo-153430/providers/microsoft.operationalinsights/workspaces/loganalytics-153430"
+
 echo "AKS cluster created: $clusterName"
 
 # Connect to AKS cluster
 
-echo "Getting AKS credentials"
+echo "Step 2 - Getting AKS credentials"
 
 az aks get-credentials \
 --resource-group $resourceGroup \
--- setup-script.sh
+++ setup-script.sh
@@ -2,7 +2,7 @@
 
 # Variables
 resourceGroup="acdnd-c4-project"
-location="westus2"
+location="westus"
 osType="UbuntuLTS"
 vmssName="udacity-vmss"
 adminName="udacityadmin"
@@ -17,8 +17,11 @@ probeName="tcpProbe"
 vmSize="Standard_B1ls"
 storageType="Standard_LRS"
 
-# Create resource group
-echo "Creating resource group $resourceGroup..."
+# Create resource group. 
+# This command will not work for the Cloud Lab users. 
+# Cloud Lab users can comment this command and 
+# use the existing Resource group name, such as, resourceGroup="cloud-demo-153430" 
+echo "STEP 0 - Creating resource group $resourceGroup..."
 
 az group create \
 --name $resourceGroup \
@@ -28,7 +31,7 @@ az group create \
 echo "Resource group created: $resourceGroup"
 
 # Create Storage account
-echo "Creating storage account $storageAccount"
+echo "STEP 1 - Creating storage account $storageAccount"
 
 az storage account create \
 --name $storageAccount \
@@ -39,7 +42,7 @@ az storage account create \
 echo "Storage account created: $storageAccount"
 
 # Create Network Security Group
-echo "Creating network security group $nsgName"
+echo "STEP 2 - Creating network security group $nsgName"
 
 az network nsg create \
 --resource-group $resourceGroup \
@@ -49,7 +52,7 @@ az network nsg create \
 echo "Network security group created: $nsgName"
 
 # Create VM Scale Set
-echo "Creating VM scale set $vmssName"
+echo "STEP 3 - Creating VM scale set $vmssName"
 
 az vmss create \
   --resource-group $resourceGroup \
@@ -71,7 +74,7 @@ az vmss create \
 echo "VM scale set created: $vmssName"
 
 # Associate NSG with VMSS subnet
-echo "Associating NSG: $nsgName with subnet: $subnetName"
+echo "STEP 4 - Associating NSG: $nsgName with subnet: $subnetName"
 
 az network vnet subnet update \
 --resource-group $resourceGroup \
@@ -83,7 +86,7 @@ az network vnet subnet update \
 echo "NSG: $nsgName associated with subnet: $subnetName"
 
 # Create Health Probe
-echo "Creating health probe $probeName"
+echo "STEP 5 - Creating health probe $probeName"
 
 az network lb probe create \
   --resource-group $resourceGroup \
@@ -98,7 +101,7 @@ az network lb probe create \
 echo "Health probe created: $probeName"
 
 # Create Network Load Balancer Rule
-echo "Creating network load balancer rule $lbRule"
+echo "STEP 6 - Creating network load balancer rule $lbRule"
 
 az network lb rule create \
   --resource-group $resourceGroup \
@@ -115,7 +118,7 @@ az network lb rule create \
 echo "Network load balancer rule created: $lbRule"
 
 # Add port 80 to inbound rule NSG
-echo "Adding port 80 to NSG $nsgName"
+echo "STEP 7 - Adding port 80 to NSG $nsgName"
 
 az network nsg rule create \
 --resource-group $resourceGroup \
@@ -129,7 +132,7 @@ az network nsg rule create \
 echo "Port 80 added to NSG: $nsgName"
 
 # Add port 22 to inbound rule NSG
-echo "Adding port 22 to NSG $nsgName"
+echo "STEP 8 - Adding port 22 to NSG $nsgName"
 
 az network nsg rule create \
 --resource-group $resourceGroup \
