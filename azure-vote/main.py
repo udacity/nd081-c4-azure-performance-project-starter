7e280355a4f37c7188594976f5305e37c6b3900f
rouvkoehler <51815354+rouvkoehler@users.noreply.github.com>
2021-09-07 18:20:19 +0200

    main.py

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -9,20 +9,35 @@ from datetime import datetime
 
 # App Insights
 # TODO: Import required libraries for App Insights
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.ext.azure.log_exporter import AzureEventHandler
+from opencensus.ext.azure import metrics_exporter
+from opencensus.stats import aggregation as aggregation_module
+from opencensus.stats import measure as measure_module
+from opencensus.stats import stats as stats_module
+from opencensus.stats import view as view_module
+from opencensus.tags import tag_map as tag_map_module
+from opencensus.trace import config_integration
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.trace.samplers import ProbabilitySampler
+from opencensus.trace.tracer import Tracer
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 
 # Logging
-logger = # TODO: Setup logger
+#logger = # TODO: Setup logger
 
 # Metrics
-exporter = # TODO: Setup exporter
+#exporter = metrics_exporter.new_metrics_exporter(
+#  enable_standard_metrics=True,
+#  connection_string='InstrumentationKey=TODO')
 
 # Tracing
-tracer = # TODO: Setup tracer
+#tracer = # TODO: Setup tracer
 
 app = Flask(__name__)
 
 # Requests
-middleware = # TODO: Setup flask middleware
+#middleware = # TODO: Setup flask middleware
 
 # Load configurations from environment or config file
 app.config.from_pyfile('config_file.cfg')
