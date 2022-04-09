30a236ce31320df6455c7ed2606d6de146823c7b
Khoa Trần <tdangkhoa@bee.local>
2022-04-10 01:52:10 +0700

    Update images

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -83,22 +83,22 @@ else:
     title = app.config['TITLE']
 
 # Redis Connection to a local server running on the same machine where the current FLask app is running.
-r = redis.Redis()
+# r = redis.Redis()
 
-"""redis_server = os.environ['REDIS']
+redis_server = os.environ['REDIS']
 
 # Redis Connection to another container
 try:
     if "REDIS_PWD" in os.environ:
         r = redis.StrictRedis(host=redis_server,
-                        port=6379,
-                        password=os.environ['REDIS_PWD'])
+                              port=6379,
+                              password=os.environ['REDIS_PWD'])
     else:
         r = redis.Redis(redis_server)
     r.ping()
 except redis.ConnectionError:
     exit('Failed to connect to Redis, terminating.')
-"""
+
 # Change title to host name to demo NLB
 if app.config['SHOWHOST'] == "true":
     title = socket.gethostname()
