7e9c30b2dbe59e30e2bb4b64821845cc9c98aefd
rqd3 <rqd3@users.noreply.github.com>
2022-04-28 09:53:06 +0200

    Update main.py

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -30,7 +30,6 @@ view_manager = stats.view_manager
 logger = logging.getLogger(__name__)
 
 handler = AzureLogHandler(connection_string='InstrumentationKey=2b48782e-a4be-42df-bf5e-08120ac51ecf')
-handler.setFormatter(logging.Formatter('%(traceId)s %(spanId)s %(message)s'))
 logger.addHandler(handler)
 
 eventHandler = AzureEventHandler(connection_string='InstrumentationKey=2b48782e-a4be-42df-bf5e-08120ac51ecf')
