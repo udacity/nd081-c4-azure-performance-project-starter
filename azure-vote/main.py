0843d2279d6e0822ce89e982fe4ecdfe55a86fd6
Sudhanshu Kulshrestha <48475411+SudKul@users.noreply.github.com>
2022-05-02 23:08:13 +0530

    Update main.py

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -98,7 +98,7 @@ def index():
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
 if __name__ == "__main__":
-    # comment line below when deploying to VMSS
-    app.run() # local
-    # uncomment the line below before deployment to VMSS
+    # TODO: Use the statement below when running locally
+    app.run() 
+    # TODO: Use the statement below before deployment to VMSS
     # app.run(host='0.0.0.0', threaded=True, debug=True) # remote
