755b4194541183bf7af64e45c549bf6ab0df2ac7
rolando.morejon@gmail.com <rolando.morejon@gmail.com>
2022-07-02 17:11:16 -0400

    Commit AKS

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -77,7 +77,18 @@ else:
     title = app.config['TITLE']
 
 # Redis Connection
-r = redis.Redis()
+#r = redis.Redis()
+redis_server = os.environ['REDIS']
+try:
+    if "REDIS_PWD" in os.environ:
+        r = redis.StrictRedis(host=redis_server,
+                           port=6379,
+                           password=os.environ['REDIS_PWD'])
+    else:
+        r = redis.Redis(redis_server)
+    r.ping()
+except redis.ConnectionError:
+    exit('Failed to connect to Redis, terminating.')
 
 # Change title to host name to demo NLB
 if app.config['SHOWHOST'] == "true":
