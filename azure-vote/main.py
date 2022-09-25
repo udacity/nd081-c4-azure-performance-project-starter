8a20561e6f7f680b0c0c59d969fa024ef7388d3b
gauthamrajkumarg <gauthamrajkumarg@gmail.com>
2022-09-25 12:01:15 +0700

    updated

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -127,6 +127,6 @@ def index():
 
 if __name__ == "__main__":
     # comment line below when deploying to VMSS
-    #app.run() # local
+    app.run() # local
     # uncomment the line below before deployment to VMSS
-    app.run(host='0.0.0.0', threaded=True, debug=True) # remote
+    # app.run(host='0.0.0.0', threaded=True, debug=True) # remote
