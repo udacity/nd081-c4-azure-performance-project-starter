cdbb872aa4d6c163de89f2eb1b629a207834e607
Amani <amani@alMacBook-Pro-alkhas-bAmani.local>
2022-12-31 12:05:14 +0300

    add app

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -9,6 +9,17 @@ from datetime import datetime
 
 # App Insights
 # TODO: Import required libraries for App Insights
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.ext.azure import metrics_exporter
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+from opencensus.stats import aggregation as aggregation_module
+from opencensus.stats import measure as measure_module
+from opencensus.stats import stats as stats_module
+from opencensus.stats import view as view_module
+from opencensus.tags import tag_map as tag_map_module
+from opencensus.trace.samplers import ProbabilitySampler
+from opencensus.trace.tracer import Tracer
 
 # Logging
 # logger = # TODO: Setup logger
