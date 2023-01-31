eb74aaab5bc6476489dcbb2436868eebc0df38b2
Apple <abinm670@mtroyal.ca>
2023-01-31 14:53:28 +0300

    Initial commit for Deploy_to_VMSS branch_a

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -131,6 +131,7 @@ def index():
             # Insert vote result into DB
             vote = request.form['vote']
             r.incr(vote,1)
+            print(vote)
 
             # Get current values
             vote1 = r.get(button1).decode('utf-8')
