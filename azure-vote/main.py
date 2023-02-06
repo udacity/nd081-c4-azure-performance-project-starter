fef197474d46a9039486a115b722d9baaa15e890
Apple <abinm670@mtroyal.ca>
2023-02-06 20:45:18 +0300

    new InstrumentationKey

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -27,7 +27,7 @@ from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 # Logging
 # logger = # TODO: Setup logger
 logger = logging.getLogger(__name__)
-logger.addHandler(AzureLogHandler(connection_string='InstrumentationKey=8d35f3ca-af03-4db6-bff1-c2740e8efb66;IngestionEndpoint=https://qatarcentral-0.in.applicationinsights.azure.com/;LiveEndpoint=https://qatarcentral.livediagnostics.monitor.azure.com/'))
+logger.addHandler(AzureLogHandler(connection_string='InstrumentationKey=50802e8a-2f3b-47d3-9403-a6aa60edf5e6;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/'))
 
 # # Metrics
 stats = stats_module.stats
@@ -35,14 +35,14 @@ view_manager = stats.view_manager
 # exporter = # TODO: Setup exporter
 exporter = metrics_exporter.new_metrics_exporter(
 enable_standard_metrics=True,
-connection_string='InstrumentationKey=8d35f3ca-af03-4db6-bff1-c2740e8efb66;IngestionEndpoint=https://qatarcentral-0.in.applicationinsights.azure.com/;LiveEndpoint=https://qatarcentral.livediagnostics.monitor.azure.com/')
+connection_string='InstrumentationKey=50802e8a-2f3b-47d3-9403-a6aa60edf5e6;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/')
 view_manager.register_exporter(exporter)
 
 # # Tracing
 #tracer = # TODO: Setup tracer
 tracer = Tracer(
  exporter=AzureExporter(
-     connection_string='InstrumentationKey=8d35f3ca-af03-4db6-bff1-c2740e8efb66;IngestionEndpoint=https://qatarcentral-0.in.applicationinsights.azure.com/;LiveEndpoint=https://qatarcentral.livediagnostics.monitor.azure.com/'),
+     connection_string='InstrumentationKey=50802e8a-2f3b-47d3-9403-a6aa60edf5e6;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/'),
  sampler=ProbabilitySampler(1.0),)
 
 
@@ -53,7 +53,7 @@ app = Flask(__name__)
 # middleware = # TODO: Setup flask middleware
 middleware = FlaskMiddleware(
  app,
- exporter=AzureExporter(connection_string="InstrumentationKey=8d35f3ca-af03-4db6-bff1-c2740e8efb66;IngestionEndpoint=https://qatarcentral-0.in.applicationinsights.azure.com/;LiveEndpoint=https://qatarcentral.livediagnostics.monitor.azure.com/"),
+ exporter=AzureExporter(connection_string="InstrumentationKey=50802e8a-2f3b-47d3-9403-a6aa60edf5e6;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/"),
  sampler=ProbabilitySampler(rate=1.0)
 )
 
@@ -112,7 +112,7 @@ def index():
             r.set(button1,0)
             r.set(button2,0)
             vote1 = r.get(button1).decode('utf-8')
-            # properties = {'custom_dimensions': {'Cats Vote': vote1}}
+            properties = {'custom_dimensions': {'Cats Vote': vote1}}
             # TODO: use logger object to log cat vote
             logger.info('Cats Vote', extra=properties)
 
@@ -139,6 +139,6 @@ def index():
 
 if __name__ == "__main__":
     # TODO: Use the statement below when running locally
-    #app.run() 
+    app.run() 
     # TODO: Use the statement below before deployment to VMSS
-    app.run(host='0.0.0.0', threaded=True, debug=True) # remote
+    #app.run(host='0.0.0.0', threaded=True, debug=True) # remote
--- setup-script.sh
+++ setup-script.sh
@@ -2,7 +2,7 @@
 
 # Variables
 resourceGroup="acdnd-c4-project"
-location="qatarcentral"
+location="eastasia"
 osType="UbuntuLTS"
 vmssName="udacity-vmss"
 adminName="udacityadmin"
